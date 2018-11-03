86c3389f82e1a6fb6ce7e9335d2b3605a7802b92
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 09:41:02 -0300

    Use antd layout

--- src/layouts/restricted/index.js
+++ src/layouts/restricted/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Layout } from 'antd';
 import Sidebar from '~/components/sidebar';
+
+const { Content } = Layout;
 
 const sidebarItems = [{ icon: 'home', name: 'Home', url: '/' }];
 
 const RestrictedLayout = ({ children }) => {
   return (
-    <div>
+    <Layout style={{ height: '100vh' }}>
       <Sidebar items={sidebarItems} />
-      <main>{children}</main>
-    </div>
+      <Layout style={{ marginLeft: 200 }}>
+        <Content
+          style={{ minHeight: '100vh', margin: '24px', overflow: 'initial' }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
