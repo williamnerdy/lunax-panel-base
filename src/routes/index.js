3528d2576c75a3a2f78f9723a9103c33eb832af5
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 10:25:58 -0300

    Add Settings Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,14 @@ import RestrictedRoute from './restricted';
 // Screens
 import LoginScreen from '~/screens/login';
 import HomeScreen from '~/screens/home';
+import SettingsScreen from '~/screens/settings';
 
 const Routes = () => (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <PublicRoute path="/login" component={LoginScreen} />
       <RestrictedRoute exact path="/" component={HomeScreen} />
+      <RestrictedRoute path="/settings" component={SettingsScreen} />
     </Switch>
   </BrowserRouter>
 );
