91c41d7cd4f1996a29f14aa1b514b676be410645
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 09:40:37 -0300

    Add antd sidebar structure

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,31 +1,38 @@
 // @flow
 import React from 'react';
+import { Layout, Menu } from 'antd';
 import { withRouter } from 'react-router-dom';
 import SidebarItem from '../sidebar-item';
-import { logoff } from '../../services/auth';
+
+const { Sider } = Layout;
 
 type Props = {
   items: Array<{ url: string, icon: string, name: string }>
 };
 
-const Sidebar = (props: Props) => {
-  const exit = () => {
-    logoff();
-    props.history.push('/login');
-  };
-
-  return (
-    <aside>
-      <ul>
-        {props.items.map((item, i) => (
-          <SidebarItem key={i} url={item.url} icon={item.icon}>
-            {item.name}
-          </SidebarItem>
-        ))}
-      </ul>
-      <button onClick={exit}>Sair</button>
-    </aside>
-  );
-};
+const Sidebar = (props: Props) => (
+  <Sider
+    style={{
+      overflow: 'auto',
+      height: '100vh',
+      position: 'fixed',
+      left: 0,
+      background: '#2b2f3e'
+    }}
+  >
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={['item-0']}
+      style={{ background: '#2b2f3e' }}
+    >
+      {props.items.map((item, i) => (
+        <SidebarItem key={i} url={item.url} icon={item.icon}>
+          {item.name}
+        </SidebarItem>
+      ))}
+    </Menu>
+  </Sider>
+);
 
 export default withRouter(Sidebar);
