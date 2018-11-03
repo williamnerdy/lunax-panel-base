66fc7966de8ac69d3a4bc01b598a86b4567c1f82
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 19:34:28 -0300

    Get settings data from API

--- src/screens/settings/index.js
+++ src/screens/settings/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Loading from '~/components/loading';
+import api from '~/services/api';
+import { API_DATA } from '~/settings';
 
-const SettingsScreen = () => (
-  <div>
-    <h1>Settings Screen</h1>
-  </div>
-);
+type State = {
+  loaded: boolean
+};
+
+class SettingsScreen extends Component<State> {
+  state = {
+    loaded: false
+  };
+
+  componentDidMount() {
+    api
+      .get(API_DATA.settings)
+      .then(() => {
+        this.setState({ loaded: true });
+      })
+      .catch(e => {
+        console.error(e);
+        alert('Error on load data!');
+      });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.loaded ? (
+          <div>
+            <h1>Settings Screen</h1>
+          </div>
+        ) : (
+          <Loading />
+        )}
+      </>
+    );
+  }
+}
 
 export default SettingsScreen;
