88418884ff501339dd4b5d59e18ed2eaaa27027a
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 19:23:24 -0300

    Sidebar links on settings.js

--- src/layouts/restricted/index.js
+++ src/layouts/restricted/index.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { Layout } from 'antd';
 import Navbar from '~/components/navbar';
 import Sidebar from '~/components/sidebar';
+import { SCREENS } from '~/settings';
 
 const { Content } = Layout;
-
-const sidebarItems = [
-  { icon: 'home', name: 'Home', url: '/' },
-  { icon: 'setting', name: 'Settings', url: '/settings' }
-];
 
 const RestrictedLayout = ({ children }) => {
   return (
     <Layout style={{ height: '100vh' }}>
       <Navbar />
       <Layout>
-        <Sidebar items={sidebarItems} />
+        <Sidebar items={SCREENS} />
         <Layout style={{ marginLeft: 200 }}>
           <Content style={{ margin: '24px', overflow: 'initial' }}>
             {children}
