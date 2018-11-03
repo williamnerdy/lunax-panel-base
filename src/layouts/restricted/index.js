39fa11d84cfaa2637d4bfdbe48219467046dd42f
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 09:03:49 -0300

    Add sidebar items

--- src/layouts/restricted/index.js
+++ src/layouts/restricted/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sidebar from '~/components/sidebar';
 
+const sidebarItems = [{ icon: 'home', name: 'Home', url: '/' }];
+
 const RestrictedLayout = ({ children }) => {
   return (
     <div>
-      <Sidebar />
+      <Sidebar items={sidebarItems} />
       <main>{children}</main>
     </div>
   );
