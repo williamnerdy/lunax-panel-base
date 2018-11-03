9b2e47a21712268a2b13fd7278ae1ad5d365860b
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 10:57:50 -0300

    Add app name

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Layout } from 'antd';
 import UserOptions from '../user-options';
+import { APP_NAME } from '~/settings';
 const { Header } = Layout;
 
 const RestrictedLayout = ({ children }) => {
   return (
     <Header className="header" style={{ color: '#fff', background: '#2b2f3e' }}>
+      <h3 style={{ float: 'left', color: '#fff' }}>{APP_NAME}</h3>
       <div style={{ float: 'right' }}>
         <UserOptions />
       </div>
--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,4 @@
 export const TOKEN_KEY = '@lunax-token';
 export const API_URL = 'https://example.com';
 export const URL_AUTH = '/auth-url/';
+export const APP_NAME = 'Lunax Panel';
