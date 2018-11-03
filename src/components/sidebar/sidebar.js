0be6e84e6d25db2dd744d7ada8f0ca013526671b
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 10:28:33 -0300

    Fix link selected

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -10,29 +10,32 @@ type Props = {
   items: Array<{ url: string, icon: string, name: string }>
 };
 
-const Sidebar = (props: Props) => (
-  <Sider
-    style={{
-      overflow: 'auto',
-      height: '100vh',
-      position: 'fixed',
-      left: 0,
-      background: '#2b2f3e'
-    }}
-  >
-    <Menu
-      theme="dark"
-      mode="inline"
-      defaultSelectedKeys={['item-0']}
-      style={{ background: '#2b2f3e' }}
+const Sidebar = (props: Props) => {
+  return (
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        background: '#2b2f3e'
+      }}
     >
-      {props.items.map((item, i) => (
-        <SidebarItem key={i} url={item.url} icon={item.icon}>
-          {item.name}
-        </SidebarItem>
-      ))}
-    </Menu>
-  </Sider>
-);
+      <Menu
+        theme="dark"
+        mode="inline"
+        selectedKeys={[props.location.pathname]}
+        defaultSelectedKeys={['/']}
+        style={{ background: '#2b2f3e' }}
+      >
+        {props.items.map(item => (
+          <SidebarItem key={item.url} url={item.url} icon={item.icon}>
+            {item.name}
+          </SidebarItem>
+        ))}
+      </Menu>
+    </Sider>
+  );
+};
 
 export default withRouter(Sidebar);
