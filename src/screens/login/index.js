d3f463ad3b264e3436d698e3e316b944a6d78076
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 19:13:33 -0300

    Fixed error on get data from props

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -6,30 +6,30 @@ import { login } from '~/services/auth';
 import { URL_AUTH, APP_NAME } from '~/settings';
 import Logo from '~/images/logo.png';
 
-const auth = (username, password) => {
-  api
-    .post(URL_AUTH, { username, password })
-    .then(({ data }) => {
-      if (data.key) {
-        login(data.key);
-        this.props.history.push('/');
-      } else {
-        alert('Error');
-      }
-    })
-    .catch(error => {
-      alert('Error!');
-      console.error(error);
-    });
-};
+const LoginScreen = props => {
+  const auth = (username, password) => {
+    api
+      .post(URL_AUTH, { username, password })
+      .then(({ data }) => {
+        if (data.key) {
+          login(data.key);
+          props.history.push('/');
+        } else {
+          alert('Error');
+        }
+      })
+      .catch(error => {
+        alert('Error!');
+        console.error(error);
+      });
+  };
 
-function LoginScreen() {
   return (
     <>
       <img style={{ marginBottom: '15px' }} src={Logo} alt={APP_NAME} />
       <LoginForm onSubmit={auth} />
     </>
   );
-}
+};
 
 export default withRouter(LoginScreen);
