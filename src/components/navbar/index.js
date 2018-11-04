e3459151ed801c54dd14c1ef25f0aa8b2584b3a6
William Cunha <williamoliveira2@hotmail.com>
2018-11-03 23:38:23 -0300

    Add logo to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Layout } from 'antd';
 import UserOptions from '../user-options';
 import { APP_NAME } from '~/settings';
+import Logo from '~/images/logo-small.png';
+
 const { Header } = Layout;
 
 const RestrictedLayout = ({ children }) => {
   return (
     <Header className="header" style={{ color: '#fff', background: '#2b2f3e' }}>
-      <h3 style={{ float: 'left', color: '#fff' }}>{APP_NAME}</h3>
+      <div style={{ float: 'left', color: '#fff' }}>
+        <img src={Logo} alt="Logo" />
+        <span style={{ marginLeft: '15px' }}>{APP_NAME}</span>
+      </div>
       <div style={{ float: 'right' }}>
         <UserOptions />
       </div>
