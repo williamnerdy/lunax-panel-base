47f703dea9f5272d253b6c340ff8e0691286b94c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-05 13:00:33 +0000

    Deploy williamnerdy/lunax-panel-base to github.com/williamnerdy/lunax-panel-base.git:gh-pages

---
+++ precache-manifest.990dad49df65f3284f14e8d6ee37d0ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be351724a52c001b2631",
+    "url": "/lunax-panel-base/static/css/main.fb23b567.chunk.css"
+  },
+  {
+    "revision": "be351724a52c001b2631",
+    "url": "/lunax-panel-base/static/js/main.be351724.chunk.js"
+  },
+  {
+    "revision": "887464665e153b53ac96",
+    "url": "/lunax-panel-base/static/css/1.64c9401d.chunk.css"
+  },
+  {
+    "revision": "887464665e153b53ac96",
+    "url": "/lunax-panel-base/static/js/1.88746466.chunk.js"
+  },
+  {
+    "revision": "da03b6041d2314a0c10a",
+    "url": "/lunax-panel-base/static/js/runtime~main.da03b604.js"
+  },
+  {
+    "revision": "86bc4401b8b0a80b1fc4be741420a964",
+    "url": "/lunax-panel-base/index.html"
+  }
+];
